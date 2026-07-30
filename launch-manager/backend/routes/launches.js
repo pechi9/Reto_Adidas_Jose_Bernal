48eabea5940cab6021cd1c6156fd9dc6a3d58f8e
Jose Bernal <est.jose.mbernal@unimilitar.edu.co>
2026-07-30 08:33:15 -0500

    Add files via upload

--- launch-manager/backend/routes/launches.js
+++ launch-manager/backend/routes/launches.js
@@ -1,1 +1,20 @@
+const express = require("express");
+const ctrl = require("../controllers/launchesController");
 
+const router = express.Router();
+
+router.get("/markets", ctrl.markets);
+
+router.get("/", ctrl.list);
+router.post("/", ctrl.create);
+
+router.get("/:id", ctrl.get);
+router.put("/:id", ctrl.update);
+router.delete("/:id", ctrl.remove);
+
+router.post("/:id/transition", ctrl.transition);
+
+router.post("/:id/assets", ctrl.addAsset);
+router.delete("/:id/assets/:assetId", ctrl.removeAsset);
+
+module.exports = router;
